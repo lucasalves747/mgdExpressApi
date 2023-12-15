b1675f565fb42e542062b7ddbcc4a5239c7c5568
lucasalves747 <134sandrinha@gmail.com>
2023-12-15 00:29:05 -0300

    deletei o cors

---
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/service/Cors.java
@@ -0,0 +1,33 @@
+package br.com.api.mgdexpress.MGD.EXPRESS.service;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Cors {
+
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedOrigins("*")
+                            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                            .allowedHeaders("*")
+                            .allowCredentials(true);
+                }
+            };
+        }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addServersItem(new Server().url("Sua nova URL"))
+                .addServersItem(new Server().url("http://localhost:8080"));
+    }
+
+}
