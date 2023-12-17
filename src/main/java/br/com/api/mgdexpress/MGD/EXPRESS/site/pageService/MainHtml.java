3ff61ffdfa7753c07299f88093c43833e2c8cd22
lucasalves747 <134sandrinha@gmail.com>
2023-12-17 06:23:23 -0300

    liberando a url controller-site-gerente/

--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/MainHtml.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/MainHtml.java
@@ -9,6 +9,8 @@ public class MainHtml {
                 <head>
                     <meta charset="UTF-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+                    
                    
                 </head>
                                
