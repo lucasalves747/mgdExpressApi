2d987b0965d2398f7a0adc47bd539ca0c5e00f93
lucasalves747 <134sandrinha@gmail.com>
2023-12-17 12:58:54 -0300

    mudando os dados necessarios  para criar o pedido

--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/MainHtml.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/MainHtml.java
@@ -92,7 +92,7 @@ public class MainHtml {
                                
                     function enviarPedido() {
                         var formData = new FormData(document.getElementById('pedidoForm'));
-                        var urlpedido = `${url}/pedidos`;
+                        var urlpedido = `${url}pedidos`;
                                
                         fetch(urlpedido, {
                             method: 'POST',
