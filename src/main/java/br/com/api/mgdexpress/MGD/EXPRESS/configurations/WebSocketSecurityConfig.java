a1f2534fb9906932c3c6a609b73836c931b3d0a1
lucasalves747 <134sandrinha@gmail.com>
2023-12-26 17:33:28 -0300

    url de criar pedido estava errada

---
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/configurations/WebSocketSecurityConfig.java
@@ -0,0 +1,17 @@
+package br.com.api.mgdexpress.MGD.EXPRESS.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+
+@Configuration
+@EnableWebSocketSecurity
+public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages
+                .simpDestMatchers("/websocket-endpoint/**").permitAll()
+                .anyMessage().authenticated();
+    }
+}
