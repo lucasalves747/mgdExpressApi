0aa74997dbb2fb8983a3b42701b88d263e8b7e9b
lucasalves747 <134sandrinha@gmail.com>
2023-12-26 19:26:01 -0300

    tirei a dependecia do socket

--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/configurations/WebSocketConfig.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/configurations/WebSocketConfig.java
@@ -1,13 +1,13 @@
 package br.com.api.mgdexpress.MGD.EXPRESS.configurations;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+/*import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocketMessageBroker*/
 public class WebSocketConfig{}/* implements WebSocketMessageBrokerConfigurer {
 
     @Override
