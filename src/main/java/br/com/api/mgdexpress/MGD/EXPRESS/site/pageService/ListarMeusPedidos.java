e6657eac8452ad5dac6621146197b14c5ad6f64c
lucasalves747 <134sandrinha@gmail.com>
2023-12-29 01:38:59 -0300

    testando o clearInterval(intervalId);

--- src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/ListarMeusPedidos.java
+++ src/main/java/br/com/api/mgdexpress/MGD/EXPRESS/site/pageService/ListarMeusPedidos.java
@@ -87,7 +87,7 @@ public class ListarMeusPedidos {
                 <nav>
                                        
                     <div>
-                        <a class="backLink" href="#" onclick="carregarPagina("https://mgdexpressapi-production.up.railway.app/site/gerente/home")">←</a>
+                        <p class="backLink" href="#" onclick="carregarPagina("https://mgdexpressapi-production.up.railway.app/site/gerente/home")">←</p>
                                      
                     </div>
                     <h2>Meus Pedidos</h2>
